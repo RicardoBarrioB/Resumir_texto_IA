d5a5eef6a2a22d2704a2a485a19432f012623f4e
RicardoBarrioB <ricardo.barrio@outlook.es>
2024-04-16 11:21:01 +0200

    .

--- IA_texto/text_project/ia/models.py
+++ IA_texto/text_project/ia/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class TextoEntrada(models.Model):
+    texto = models.TextField()
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+
+class Resumen(models.Model):
+    texto_entrada = models.OneToOneField(TextoEntrada, on_delete=models.CASCADE)
+    resumen = models.TextField()
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+
+
+
--- IA_texto/text_project/ia/views.py
+++ IA_texto/text_project/ia/views.py
@@ -1,3 +1,15 @@
+from django.views.generic import TemplateView
 from django.shortcuts import render
 
-# Create your views here.
+class HomeView(TemplateView):
+    template_name = 'ia/home.html'
+
+    def post(self, request, *args, **kwargs):
+        texto = request.POST.get('texto', '')  # Obtener el texto del formulario
+        # Aqui hay que hacer el resumen del texto
+        return render(request, self.template_name, {'texto': texto})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
